0ae9ea4f095fcd9db06324c05f5e9b69d82798ed
tkap44 <108929459+tkap44@users.noreply.github.com>
2022-08-07 15:43:59 +0300

    Update frontend_testing.py

--- frontend_testing.py
+++ frontend_testing.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 
 driver = webdriver.Chrome(executable_path="/Users/tkaplan/Code/Python/Automation/chromedriver")
 
-driver.get("http://127.0.0.1:5001/users/2")
+driver.get("http://127.0.0.1:5001/users/1")
 
 try:
     user = driver.find_element('id', 'user')
